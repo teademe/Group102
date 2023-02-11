8d01973b1d2f64df6bec0139ee68a80d23a0b50a
teademe <thea_pls@yahoo.com>
2023-02-11 16:31:46 -0500

    Gr.102

---
+++ Day 1/Lesson1.py
@@ -0,0 +1,12 @@
+name = "Tea"
+lastname = "Demetrashvili"
+age = 42
+height = 174
+
+print(name)
+print(lastname)
+print(name+" "+lastname)
+print(name + " " +str(age))
+
+print(lastname+" "+name+" "+ str(age)+" "+ str(height))
+print(lastname+" "+name+";"+ str(age)+";"+ str(height))
