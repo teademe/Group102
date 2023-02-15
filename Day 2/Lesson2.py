4f5d357f4769f7e50528741b27a7c2f99804c9fa
teademe <thea_pls@yahoo.com>
2023-02-14 19:23:45 -0500

    string commit

---
+++ Day 2/Lesson2.py
@@ -0,0 +1,44 @@
+name="Tea"
+last_name="Demetrashvili"
+full_name="Tea Demetrashvili"
+
+print(len(name))
+print(len(last_name))
+print(last_name[0])
+print(name[1])
+print(full_name[1:7])
+print(full_name[2:9:2])
+print(full_name[:5])
+print(full_name[5:])
+
+print(full_name[-5])
+print(name[-2]+"ABC")
+print(full_name[-1:-10:-12])
+
+name2="  TEA  "
+print(name2)
+print(full_name.replace("e","E"))
+
+
+
+
+
+
+my_sentence = "I work hard to study technologies"
+
+print(my_sentence)
+print(len(my_sentence))
+print(my_sentence.replace("t","T"))
+
+
+
+
+# if 
+
+knows_enlish=True
+
+if 12>2:
+    print("enjoy")
+# if 12<2:
+#     print("don't give up")
+
