217b1368500d5921fe2419cc1df28f5dd4b11f8c
teademe <thea_pls@yahoo.com>
2023-02-15 21:10:46 -0500

    lesson3 unfinished

---
+++ Day 3/Lesson3.py
@@ -0,0 +1,66 @@
+# Class work 1
+
+# name="Tea"
+# lastname="Demetrashvili"
+# age=42
+# height=174
+# Date_of_birth=1980
+# place_of_birth="Tbilisi,Georgia"
+
+# my_sentence = "N {0} LN {1} Age {3} H {2} DOB {4} POB {5}". format(name, lastname, age,height,Date_of_birth,place_of_birth)
+# print(my_sentence)
+
+# end of the class work 1
+
+
+
+
+# # clas work 2
+
+# my_name=input("enter your name: ")
+# my_lastname=input("enter your lastname: ")
+# my_age=int(input("your age: "))
+
+# print("my_name {} my_lastname {} my_age {}". format(my_name, my_lastname, my_age))
+
+# my_age+=3
+# print("after 3 years I will be {} years old". format(my_age))
+
+# end of class work 2
+
+
+
+
+# # class work 3
+
+# num1=int(input("enter num1:" ))
+# num2=int(input("enter num2:" ))
+# product=num1*num2
+
+# if product>100:
+#     print(product)
+# else:
+#     print("you lost")
+
+# end class work 3
+
+
+#homework
+
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+num3=int(input("enter num3: "))
+product=num1+num2+num3
+if "num1" "num2" "num3" /2==odd_number:
+    print(product)
+
+
+
+
+
+
+
+
+
+
+
