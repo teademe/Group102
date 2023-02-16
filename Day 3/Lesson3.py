e28ce759eea3219077e21a464d16db7a23d04383
teademe <thea_pls@yahoo.com>
2023-02-16 15:01:33 -0500

    homework

--- Day 3/Lesson3.py
+++ Day 3/Lesson3.py
@@ -1,14 +1,14 @@
 # Class work 1
 
-# name="Tea"
-# lastname="Demetrashvili"
-# age=42
-# height=174
-# Date_of_birth=1980
-# place_of_birth="Tbilisi,Georgia"
+name="Tea"
+lastname="Demetrashvili"
+age=42
+height=174
+Date_of_birth=1980
+place_of_birth="Tbilisi,Georgia"
 
-# my_sentence = "N {0} LN {1} Age {3} H {2} DOB {4} POB {5}". format(name, lastname, age,height,Date_of_birth,place_of_birth)
-# print(my_sentence)
+my_sentence = "N {0} LN {1} Age {3} H {2} DOB {4} POB {5}". format(name, lastname, age,height,Date_of_birth,place_of_birth)
+print(my_sentence)
 
 # end of the class work 1
 
@@ -17,14 +17,14 @@
 
 # # clas work 2
 
-# my_name=input("enter your name: ")
-# my_lastname=input("enter your lastname: ")
-# my_age=int(input("your age: "))
+my_name=input("enter your name: ")
+my_lastname=input("enter your lastname: ")
+my_age=int(input("your age: "))
 
-# print("my_name {} my_lastname {} my_age {}". format(my_name, my_lastname, my_age))
+print("my_name {} my_lastname {} my_age {}". format(my_name, my_lastname, my_age))
 
-# my_age+=3
-# print("after 3 years I will be {} years old". format(my_age))
+my_age+=3
+print("after 3 years I will be {} years old". format(my_age))
 
 # end of class work 2
 
@@ -33,14 +33,14 @@
 
 # # class work 3
 
-# num1=int(input("enter num1:" ))
-# num2=int(input("enter num2:" ))
-# product=num1*num2
+num1=int(input("enter num1:" ))
+num2=int(input("enter num2:" ))
+product=num1*num2
 
-# if product>100:
-#     print(product)
-# else:
-#     print("you lost")
+if product>100:
+    print(product)
+else:
+    print("you lost")
 
 # end class work 3
 
@@ -56,7 +56,7 @@ sum=num1+num2+num3
 if num1 % 2 == 1 and num2 % 2 == 1 and num3 % 2 == 1:
     print(sum)
 else:
-    print("sorry_for_the_inconvinience")
+    print("sorry for the inconvinience")
 
 
 
